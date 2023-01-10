a3fa22b811730078569358e17d9a217378209dd9
Vladislav Kondrashov <vlad.a.kondrashov@gmail.com>
2023-01-10 15:17:25 +0400

    Fixed options frame loading process

--- Options.lua
+++ Options.lua
@@ -17,27 +17,57 @@ function MCF_OptionsFrame_OnLoad(self)
     _G[self:GetName().."SubText"]:SetText(L["MCF_OPTIONS_DESCRIPTION"]);
     _G[self:GetName().."TacoTipIntegrationTitle"]:SetText(L["MCF_OPTIONS_TT_INTEGRATION_TITLE"]);
 end
+function MCF_OptionsFrame_Update()
+    local optionsFrame = MCF_OptionsFrame;
+    local TTIntegration = _G[optionsFrame:GetName().."TacoTipIntegration"];
+    local TTEnableButton = _G[TTIntegration:GetName().."EnableButton"];
+    local TTGSDropDown = _G[TTIntegration:GetName().."GSStatTypeDropDown"];
+    local TTColorButton = _G[TTIntegration:GetName().."GSColorEnableButton"];
+
+    if ( not MCF_GetSettings("TT_IntegrationEnabled") ) then
+        -- TT Integration Enable button
+        TTEnableButton:SetChecked(false);
+    else
+        TTEnableButton:SetChecked(true);
+    end
+
+    TTGSDropDown:SetValue(TTGSDropDown.defaultValue);
+    MCF_OptionsFrameGSTextTypeDropDown_OnLoad(TTGSDropDown);
+
+    if ( not MCF_GetSettings("TT_IntegrationColorEnabled") ) then
+        TTColorButton:SetChecked(false);
+    else
+        TTColorButton:SetChecked(true);
+    end
+
+    if ( not IsAddOnLoaded("TacoTip") ) then
+        TTEnableButton:Disable();
+        TTEnableButton.Text:SetFontObject(GameFontDisable);
+
+        UIDropDownMenu_DisableDropDown(TTGSDropDown);
+
+        TTColorButton:Disable();
+        TTColorButton.Text:SetFontObject(GameFontDisable);
+    elseif ( TTEnableButton:GetChecked() ) then
+        UIDropDownMenu_EnableDropDown(TTGSDropDown);
+
+        TTColorButton:Enable();
+        TTColorButton.Text:SetFontObject(GameFontHighlight);
+    end
+end
 
 -- TACOTIP INTEGRATION ENABLE BUTTON FUNCTIONS
 function MCF_OptionsFrameTTEnableButton_OnLoad(self)
     self.Text:SetText(L["MCF_OPTIONS_TT_INTEGRATION_ENABLE_LABEL"]);
     self.tooltip = L["MCF_OPTIONS_TT_INTEGRATION_ENABLE_TOOLTIP"];
 
-    self:RegisterEvent("ADDON_LOADED");
+    if ( not MCF_GetSettings("TT_IntegrationEnabled") ) then
+        self:SetChecked(false);
+    else
+        self:SetChecked(true);
+    end
 
     self:HookScript("OnClick", function(self) MCF_OptionsFrameTTEnableButton_HookOnClick(self); end);
-end
-function MCF_OptionsFrameTTEnableButton_OnEvent(self, event, ...)
-    local arg = ...;
-
-    if ( (event == "ADDON_LOADED") and (arg == "ModernCharacterFrame") ) then
-        if ( not MCF_GetSettings("TT_IntegrationEnabled") ) then
-            self:SetChecked(false);
-        else
-            self:SetChecked(true);
-        end
-        self:UnregisterEvent(event);
-    end
 end
 function MCF_OptionsFrameTTEnableButton_OnShow(self)
     if ( not IsAddOnLoaded("TacoTip") ) then
@@ -63,74 +93,55 @@ end
 
 -- TACOTIP GEARSCORE TEXT TYPE DROPDOWN MENU FUNCTIONS
 function MCF_OptionsFrameGSTextTypeDropDown_OnLoad(self)
-    self:RegisterEvent("ADDON_LOADED");
     _G[self:GetName().."Label"]:SetText(L["MCF_OPTIONS_TT_INTEGRATION_GSTYPE_LABEL"]);
+
+    self.defaultValue = 1;
+    self.oldValue = MCF_GetSettings("TT_IntegrationType");
+    self.value = self.oldValue;
+    self.tooltip = L["MCF_OPTIONS_TT_INTEGRATION_GSTYPE_TOOLTIP"];
+
+    UIDropDownMenu_SetWidth(self, 90);
+    UIDropDownMenu_Initialize(self, MCF_OptionsFrameGSTextTypeDropDown_Initialize);
+    UIDropDownMenu_SetSelectedValue(self, self.value);
+
+    self.SetValue =
+        function(self, value)
+            self.value = value;
+            UIDropDownMenu_SetSelectedValue(self, value);
+            MCF_SetSettings("TT_IntegrationType", value);
+        end;
+    self.GetValue =
+        function(self)
+            return UIDropDownMenu_GetSelectedValue(self);
+        end;
+    self.RefreshValue =
+        function(self)
+            UIDropDownMenu_Initialize(self, MCF_OptionsFrameGSTextTypeDropDown_Initialize);
+            UIDropDownMenu_SetSelectedValue(self, self.value);
+        end;
 end
 function MCF_OptionsFrameGSTextTypeDropDown_Initialize()
     local selectedValue = UIDropDownMenu_GetSelectedValue(MCF_OptionsFrameTacoTipIntegrationGSStatTypeDropDown);
     local info = UIDropDownMenu_CreateInfo();
 
-    info.text = "ILVL / GS";
     info.func = MCF_OptionsFrameGSTextTypeDropDown_OnClick;
+
+    info.text = "ILVL / GS";
     info.value = 1;
-    if (info.value == selectedValue) then
-        info.checked = 1;
-    else
-        info.checked = nil;
-    end
+    info.checked = info.value == selectedValue;
     UIDropDownMenu_AddButton(info);
 
     info.text = "ILVL (GS)";
-    info.func = MCF_OptionsFrameGSTextTypeDropDown_OnClick;
     info.value = 2;
-    if (info.value == selectedValue) then
-        info.checked = 1;
-    else
-        info.checked = nil;
-    end
+    info.checked = info.value == selectedValue;
     UIDropDownMenu_AddButton(info);
 
     info.text = "GS";
-    info.func = MCF_OptionsFrameGSTextTypeDropDown_OnClick;
     info.value = 3;
-    if (info.value == selectedValue) then
-        info.checked = 1;
-    else
-        info.checked = nil;
-    end
+    info.checked = info.value == selectedValue;
     UIDropDownMenu_AddButton(info);
 
 
-end
-function MCF_OptionsFrameGSTextTypeDropDown_OnEvent(self, event, ...)
-    local arg = ...;
-    if ( (event == "ADDON_LOADED") and (arg == "ModernCharacterFrame") ) then
-        self.defaultValue = 1;
-        self.oldValue = MCF_GetSettings("TT_IntegrationType");
-        self.value = self.defaultValue;
-        self.tooltip = L["MCF_OPTIONS_TT_INTEGRATION_GSTYPE_TOOLTIP"];
-
-        UIDropDownMenu_SetWidth(self, 90);
-        UIDropDownMenu_Initialize(self, MCF_OptionsFrameGSTextTypeDropDown_Initialize);
-        UIDropDownMenu_SetSelectedValue(self, self.value);
-
-        self.SetValue =
-            function(self, value)
-                self.value = value;
-                UIDropDownMenu_SetSelectedValue(self, value);
-                MCF_SetSettings("TT_IntegrationType", value);
-            end;
-        self.GetValue =
-            function(self)
-                return UIDropDownMenu_GetSelectedValue(self);
-            end;
-        self.RefreshValue =
-            function(self)
-                UIDropDownMenu_Initialize(self, MCF_OptionsFrameGSTextTypeDropDown_Initialize);
-                UIDropDownMenu_SetSelectedValue(self, self.value);
-            end;
-        self:UnregisterEvent(event);
-    end
 end
 function MCF_OptionsFrameGSTextTypeDropDown_OnShow(self)
     local EnableButton = _G[self:GetParent():GetName().."EnableButton"];
@@ -148,21 +159,13 @@ function MCF_OptionsFrameGSColorEnableButton_OnLoad(self)
     self.Text:SetText(L["MCF_OPTIONS_TT_INTEGRATION_COLOR_LABEL"]);
     self.tooltip = L["MCF_OPTIONS_TT_INTEGRATION_COLOR_TOOLTIP"];
 
-    self:RegisterEvent("ADDON_LOADED");
+    if ( not MCF_GetSettings("TT_IntegrationColorEnabled") ) then
+        self:SetChecked(false);
+    else
+        self:SetChecked(true);
+    end
 
     self:HookScript("OnClick", function(self) MCF_OptionsFrameGSColorEnableButton_HookOnClick(self); end);
-end
-function MCF_OptionsFrameGSColorEnableButton_OnEvent(self, event, ...)
-    local arg = ...;
-
-    if ( (event == "ADDON_LOADED") and (arg == "ModernCharacterFrame") ) then
-        if ( not MCF_GetSettings("TT_IntegrationColorEnabled") ) then
-            self:SetChecked(false);
-        else
-            self:SetChecked(true);
-        end
-        self:UnregisterEvent(event);
-    end
 end
 function MCF_OptionsFrameGSColorEnableButton_OnShow(self)
     local EnableButton = _G[self:GetParent():GetName().."EnableButton"];
@@ -196,6 +199,7 @@ function MCF_OptionsFrameResetSettingsButton_OnClick(self)
         button2 = NO,
         OnAccept = function()
             MCF_ResetSettings();
+            MCF_OptionsFrame_Update();
             SendSystemMessage(L["MCF_OPTIONS_RESETED_MESSAGE"]);
             self:Disable();
         end,
