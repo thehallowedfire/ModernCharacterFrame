bfc72cd513438a4ab7cba47fe247ee9324881de1
Vladislav Kondrashov <vlad.a.kondrashov@gmail.com>
2023-03-03 17:54:29 +0400

    Add new option setting

--- DBSettings.lua
+++ DBSettings.lua
@@ -9,9 +9,24 @@ MCF_DEFAULT_SETTINGS = {
 
 	["enableItemSlotColoring"] = true,
 
+	["showRepairCost"] = true,
+
 	["TT_IntegrationEnabled"] = true,
 	["TT_IntegrationType"] = 1,
 	["TT_IntegrationColorEnabled"] = true,
+
+	["setSpecs"] = {
+		[0] = false,
+		[1] = false,
+		[2] = false,
+		[3] = false,
+		[4] = false,
+		[5] = false,
+		[6] = false,
+		[7] = false,
+		[8] = false,
+		[9] = false
+	},
 }
 
 function MCF_SetSettings(setting, value, id)
